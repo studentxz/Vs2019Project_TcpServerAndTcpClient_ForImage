51c88f4ab9c10a9d1af6b61b9aa629f540f4aa8a
studentxz <308795606@qq.com>
2019-09-25 09:45:53 +0800

    first commit!

---
+++ TCPClientForImage/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClientForImage
+{
+    class Program
+    {
+
+        static int Port = 63999;
+
+        static void Main(string[] args)
+        {
+            SendImageToServer(@"E:\个人\本人.jpg");
+        }
+
+        static void SendImageToServer(string imgURI)
+        {
+            if (!File.Exists(imgURI))
+            {
+                //停止函数
+                return;
+            }
+
+            //创建一个文件流打开图片
+            FileStream fileStream = new FileStream(imgURI, FileMode.Open);
+
+            //声明一个byte[]数组接收图片字节信息
+            byte[] fileBytes = new byte[fileStream.Length];
+
+            using (fileStream)
+            {
+                fileStream.Read(fileBytes, 0, fileBytes.Length);
+                fileStream.Close();
+            }
+
+            //服务器IP地址
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+
+            //创建TCPClient对象，以实现与服务器的连接。
+            TcpClient tcpClient = new TcpClient();
+
+            //连接服务器
+            tcpClient.Connect(iPAddress, Port);
+
+            using (tcpClient)
+            {
+                //连接完服务器后在客户端和服务端之间产生一个流的通道
+                NetworkStream networkStream = tcpClient.GetStream();
+
+                using (networkStream)
+                {
+                    //将图片的字节数据写入流中
+                    networkStream.Write(fileBytes, 0, fileBytes.Length);
+                }
+            }
+        }
+    }
+}
---
+++ VideoReceiver/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VideoReceiver
+{
+    class Program
+    {
+        static TcpClient client;
+        
+
+        //static string IP = "127.0.0.1";
+        static int Port = 63999;
+
+        static int bufferLength = 200;
+        static byte[] buffer = new byte[bufferLength];
+
+        static NetworkStream networkStream;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("TcpServer is already start!"+ DateTime.Now.ToString());
+            Console.WriteLine("等待客户端连接" + DateTime.Now.ToString());
+
+            ConnectAndListen();
+            
+        }
+
+        static async void ConnectAndListen()
+        {
+
+            //建立监听对象，监听任何ip，端口为63999
+            TcpListener listener = new TcpListener(IPAddress.Any, Port);
+
+            //开始监听
+            listener.Start();
+
+            while (true)
+            {
+                
+                //线程会挂在在这儿，直到客户端发来连接请求
+                client = listener.AcceptTcpClient();
+
+                Console.WriteLine("客户端已连接"+client.Client.RemoteEndPoint+DateTime.Now.ToString());
+
+                //得到从客户端传来的网络流
+                networkStream = client.GetStream();
+
+                DateTime Now = DateTime.Now;
+
+                FileStream fileStream = new FileStream(
+                @"D:\vsProject\VS2019Project\FileStorage\" + Now.Year + Now.Month + Now.Day + Now.Hour + Now.Minute + Now.Second + ".jpg", FileMode.Create);
+
+                //如果网络流中有数据
+                while (networkStream.DataAvailable)
+                {
+                    
+                    //异步读取网络流中的byte信息
+                    var byteLength = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                    //异步写入数据到文件流中
+                    fileStream.Write(buffer, 0, byteLength);
+
+                }
+
+                //判断是否和客户端还存在连接
+                if (!IsConnected(client))
+                {
+                    Console.WriteLine("已经与客户端断开连接" + DateTime.Now.ToString());
+                }
+
+                client.Close();
+                fileStream.Dispose();
+                fileStream.Close();
+                networkStream.Dispose();
+
+            }
+        }
+
+        /// <summary>
+        /// 判断socket是否还连接
+        /// </summary>
+        /// <param name="tcpClient"></param>
+        /// <returns></returns>
+        static bool IsConnected(TcpClient tcpClient)
+        {
+            return !((tcpClient.Client.Poll(1000, SelectMode.SelectRead) && (tcpClient.Client.Available == 0)) || !tcpClient.Client.Connected);
+        }
+    }
+}
